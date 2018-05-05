78b25b1df27ee45d45a0663bd5611964cf2e9d3d
Elvis de Freitas <edigitalb@gmail.com>
2018-05-05 17:01:42 -0300

    #27 fixing test compile

--- bk-api/controller/api/bookmark_test.go
+++ bk-api/controller/api/bookmark_test.go
@@ -2,12 +2,13 @@ package api
 
 import (
 	"testing"
-	"github.com/mageddo/go-logging"
 	"bk-api/test"
 	"bk-api/service"
 	"bk-api/entity"
 	"github.com/stretchr/testify/assert"
+	"regexp"
 )
+var regex = regexp.MustCompile("[\t\n]+")
 
 func TestGetV1_0ListBookmarksNoOffsetError(t *testing.T){
 
@@ -26,14 +27,13 @@ func TestGetV1_0ListBookmarksNoOffsetError(t *testing.T){
 
 func TestGetV1_0ListBookmarks(t *testing.T){
 
-	ctx := logging.NewContext()
 	test.BuildDatabase()
 
 	expectedBookmarks := "[{\"id\":1,\"name\":\"X\",\"visibility\":1,\"html\":\"\",\"length\":3}]"
 
-	service.NewBookmarkService(ctx).SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X", entity.PUBLIC))
-	service.NewBookmarkService(ctx).SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X2", entity.PUBLIC))
-	service.NewBookmarkService(ctx).SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X3", entity.PRIVATE))
+	service.NewBookmarkService().SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X", entity.PUBLIC))
+	service.NewBookmarkService().SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X2", entity.PUBLIC))
+	service.NewBookmarkService().SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X3", entity.PRIVATE))
 
 	resp, c, err := test.NewReq("GET", BOOKMARK_V1 + "?from=0&quantity=1")
 
@@ -49,14 +49,13 @@ func TestGetV1_0ListBookmarks(t *testing.T){
 
 func TestGetV1_0ListBookmarksValidateFromSuccess(t *testing.T){
 
-	ctx := logging.NewContext()
 	test.BuildDatabase()
 
 	expectedBookmarks := "[{\"id\":2,\"name\":\"X2\",\"visibility\":1,\"html\":\"\",\"length\":3},{\"id\":3,\"name\":\"X3\",\"visibility\":0,\"html\":\"\",\"length\":3}]"
 
-	service.NewBookmarkService(ctx).SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X", entity.PUBLIC))
-	service.NewBookmarkService(ctx).SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X2", entity.PUBLIC))
-	service.NewBookmarkService(ctx).SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X3", entity.PRIVATE))
+	service.NewBookmarkService().SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X", entity.PUBLIC))
+	service.NewBookmarkService().SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X2", entity.PUBLIC))
+	service.NewBookmarkService().SaveBookmark(entity.NewBookmarkWithNameAndVisibility("X3", entity.PRIVATE))
 
 	resp, c, err := test.NewReq("GET", BOOKMARK_V1 + "?from=1&quantity=2")
 
@@ -72,14 +71,13 @@ func TestGetV1_0ListBookmarksValidateFromSuccess(t *testing.T){
 
 func TestGetV1_0ListBookmarksSearchSuccess(t *testing.T){
 
-	ctx := logging.NewContext()
 	test.BuildDatabase()
 
 	expectedBookmarks := `[{"id":2,"name":"Android 7.0 was released","html":"","length":2},{"id":3,"name":"Separate your software release by major, minor and patch","html":"","length":2}]`
 
-	service.NewBookmarkService(ctx).SaveBookmark(entity.NewBookmarkWithNameAndVisibility("Google is the most popular search engine site", entity.PUBLIC))
-	service.NewBookmarkService(ctx).SaveBookmark(entity.NewBookmarkWithNameAndVisibility("Android 7.0 was released", entity.PUBLIC))
-	service.NewBookmarkService(ctx).SaveBookmark(entity.NewBookmarkWithNameAndVisibility("Separate your software release by major, minor and patch", entity.PRIVATE))
+	service.NewBookmarkService().SaveBookmark(entity.NewBookmarkWithNameAndVisibility("Google is the most popular search engine site", entity.PUBLIC))
+	service.NewBookmarkService().SaveBookmark(entity.NewBookmarkWithNameAndVisibility("Android 7.0 was released", entity.PUBLIC))
+	service.NewBookmarkService().SaveBookmark(entity.NewBookmarkWithNameAndVisibility("Separate your software release by major, minor and patch", entity.PRIVATE))
 
 	resp, c, err := test.NewReq("GET", BOOKMARK_V1 + "?from=0&quantity=3&query=release")
 
