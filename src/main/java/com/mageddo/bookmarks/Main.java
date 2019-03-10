52d0f47d3616aab69e832267f5331b1161114cd3
Elvis de Freitas <edigitalb@gmail.com>
2019-03-09 22:36:32 -0300

    setup java project with graalvm support

---
+++ src/main/java/com/mageddo/bookmarks/Main.java
@@ -0,0 +1,38 @@
+package com.mageddo.bookmarks;
+
+import static spark.Spark.*;
+
+public class Main {
+	public static void main(String[] args) {
+
+		get("/hello", (request, response) -> "Hello World!");
+
+		post("/hello", (request, response) ->
+			"Hello World: " + request.body()
+		);
+
+		get("/private", (request, response) -> {
+			response.status(401);
+			return "Go Away!!!";
+		});
+
+		get("/users/:name", (request, response) -> "Selected user: " + request.params(":name"));
+
+		get("/news/:section", (request, response) -> {
+			response.type("text/xml");
+			return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><news>" + request.params("section") + "</news>";
+		});
+
+		get("/protected", (request, response) -> {
+			halt(403, "I don't think so!!!");
+			return null;
+		});
+
+		get("/redirect", (request, response) -> {
+			response.redirect("/news/world");
+			return null;
+		});
+
+		get("/", (request, response) -> "root");
+	}
+}
