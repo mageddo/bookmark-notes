44ff3436cd38c050817270cbc4d5802b29cb58e0
Elvis de Freitas <edigitalb@gmail.com>
2019-03-23 14:02:26 -0300

    fixing flyway migration

--- src/main/java/com/mageddo/utils/MigrationUtils.java
+++ src/main/java/com/mageddo/utils/MigrationUtils.java
@@ -2,19 +2,40 @@ package com.mageddo.utils;
 
 import com.mageddo.config.DatasourceConfiguration;
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.graalvm.nativeimage.ImageInfo;
+import org.graalvm.nativeimage.ProcessProperties;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 public final class MigrationUtils {
 	private MigrationUtils() {
 	}
 
 	public static void migrate(DatasourceConfiguration dc){
-		final Flyway flyway = Flyway
+
+		FluentConfiguration configuration = Flyway
 			.configure()
-			.locations(System.getenv("MIGRATION_PATH"))
-//			.locations("classpath:db/migration/postgres")
-			.dataSource(dc.getJdbcUrl(), dc.getUsername(), dc.getPassword())
-			.load();
+//			.configure(getClassLoader())
+//			.configuration(Flyway.configure())
+//			.locations(System.getenv("MIGRATION_PATH"))
+//			.resolvers()
+			.locations("classpath:db/migration/postgres")
+			.dataSource(dc.getJdbcUrl(), dc.getUsername(), dc.getPassword());
+//			.load();
+
+		if(ImageInfo.inImageRuntimeCode()){
+			final Path path = Paths.get(ProcessProperties.getExecutableName()).getParent();
+			configuration = configuration.locations(String.format("filesystem:%s/conf/db/migration/postgres", path));
+		}
+
+		final Flyway flyway = configuration.load();
 		flyway.repair();
 		flyway.migrate();
 	}
+
 }
