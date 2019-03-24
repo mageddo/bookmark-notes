ba8dc34cf17f0fc75a7562516d4a45641c279d38
Elvis de Freitas <edigitalb@gmail.com>
2019-03-24 01:30:50 -0300

    go bk-api was replaced by java version

--- bk-api/controller/app/settings.go
+++
@@ -1,6 +0,0 @@
-package app
-
-func init(){
-
-}
-
--- bk-api/dao/utils_dao.go
+++
@@ -1,9 +0,0 @@
-package dao
-
-type UtilsDAO interface {
-	Exec(sql string)
-}
-
-func NewUtilsDAO() *UtilsDAOSQLite {
-	return &UtilsDAOSQLite{}
-}
