4c38c823e6535610df17d5c1b6a39d2c15e34f66
Elvis de Freitas <edigitalb@gmail.com>
2019-03-24 12:51:34 -0300

    migrating restricted area home template

--- src/main/java/com/mageddo/bookmarks/controller/HomeController.java
+++ src/main/java/com/mageddo/bookmarks/controller/HomeController.java
@@ -33,17 +33,17 @@ public class HomeController {
 
 	@Get
 	@View("index")
-	HttpResponse index(@QueryValue(value = "page", defaultValue = "1") int page){
+	HttpResponse index(@QueryValue(value = "page", defaultValue = "1") int page) {
 		page -= 1;
 		final int size = bookmarksService.countPublicNotDeleted();
 		int pageSize = 10, pages = (int) Math.ceil(size / (double) pageSize), startPage = 0;
-		if (page + 1 > pages){
+		if (page + 1 > pages) {
 			return ok(mapOf(
 				"headerTitle", "Not found",
 				"msg", "No results",
 				"getURL", (Function<String, String>) URLUtils::getURL
 			));
-		} else if(page > 0){
+		} else if (page > 0) {
 			startPage = page * pageSize;
 		}
 		logger.debug("start=%d, size=%d, page=%d, pages=%d", startPage, size, page, pages);
@@ -60,133 +60,22 @@ public class HomeController {
 			"toTagArray", (Function<String, String[]>) tags -> tags.equals("{NULL}") ? null : tags.split(","),
 			"getURL", (Function<String, String>) URLUtils::getURL
 		));
-//		getURL(){
-//			return function(path, render){
-//				return utils.getSEOURL(req, render(path))
-//			};
-//		}
 	}
-//	app.get("/", function(req, res){
-//
-//	var page = parseInt(url.parse(req.url, true).query.page || 1) - 1;
-//	m.countPublicNotDeletedBookmarks(function(err, size){
-//		if(err){
-//			return res.render("503", {
-//				layout: false
-//			});
-//		}
-//
-//		var pageSize = 10, pages = Math.ceil(size / pageSize), startPage = 0;
-//		if (page + 1 > pages){
-//			return res.render("index", {
-//				title: "Not found",
-//				layout: "publicLayout",
-//				msg: "No results",
-//				getURL(){
-//				return function(path, render){
-//					return utils.getURL(req, render(path))
-//				};
-//			}
-//			});
-//		} else if(page > 0){
-//			startPage = page * pageSize;
-//		}
-//
-//		console.debug("m=getRecentBookmarks, start=%d, size=%d, page=%d, pages=%d", startPage, size, page, pages)
-//		m.getRecentBookmarks(pageSize, startPage, function(err, data){
-//			console.debug("m=getRecentBookmarks, err=%j", err)
-//			if(err){
-//				return res.render("503", {
-//					layout: false
-//				});
-//			}
-//			return res.render("index", {
-//				analytics: config.get("analytics.id"),
-//				title: page == 0 ? "Home" : "Page " + (page + 1),
-//				pageTitle: page == 0 ? null : "Page " + (page + 1),
-//				description: "Technology posts based on my practice experience",
-//				nextPage: page + 2,
-//				hasMore: pages > page + 1,
-//				layout: "publicLayout",
-//				bookmarks: data,
-//				toTagArray(){
-//				return function(render){
-//					if(this.tags){
-//						this.tags = this.tags.split(",");
-//					}
-//				}
-//			},
-//			getURL(){
-//				return function(path, render){
-//					return utils.getSEOURL(req, render(path))
-//				};
-//			}
-//			});
-//		})
-//
-//	})
-//
-//});
 
-//	app.get("/", function(req, res){
-//
-//		var page = parseInt(url.parse(req.url, true).query.page || 1) - 1;
-//		m.countPublicNotDeletedBookmarks(function(err, size){
-//			if(err){
-//				return res.render("503", {
-//					layout: false
-//				});
-//			}
-//
-//			var pageSize = 10, pages = Math.ceil(size / pageSize), startPage = 0;
-//			if (page + 1 > pages){
-//				return res.render("index", {
-//					title: "Not found",
-//					layout: "publicLayout",
-//					msg: "No results",
-//					getURL(){
-//					return function(path, render){
-//						return utils.getURL(req, render(path))
-//					};
-//				}
-//				});
-//			} else if(page > 0){
-//				startPage = page * pageSize;
-//			}
-//
-//			console.debug("m=getRecentBookmarks, start=%d, size=%d, page=%d, pages=%d", startPage, size, page, pages)
-//			m.getRecentBookmarks(pageSize, startPage, function(err, data){
-//				console.debug("m=getRecentBookmarks, err=%j", err)
-//				if(err){
-//					return res.render("503", {
-//						layout: false
-//					});
-//				}
-//				return res.render("index", {
-//					analytics: config.get("analytics.id"),
-//					title: page == 0 ? "Home" : "Page " + (page + 1),
-//					pageTitle: page == 0 ? null : "Page " + (page + 1),
-//					description: "Technology posts based on my practice experience",
-//					nextPage: page + 2,
-//					hasMore: pages > page + 1,
-//					layout: "publicLayout",
-//					bookmarks: data,
-//					toTagArray(){
-//					return function(render){
-//						if(this.tags){
-//							this.tags = this.tags.split(",");
-//						}
-//					}
-//				},
-//				getURL(){
-//					return function(path, render){
-//						return utils.getSEOURL(req, render(path))
-//					};
-//				}
-//				});
-//			})
-//
-//		})
-//
-//	});
+	@Get("/_/bookmarks")
+	@View("restricted-area/index")
+	HttpResponse privateAreaIndex(@QueryValue(value = "page", defaultValue = "1") int page) {
+		if(false){
+			return ok(mapOf(
+				"layout", false,
+				"compile", false
+			));
+		} else {
+			return ok(mapOf(
+				"layout", true,
+				"compile", false
+			));
+		}
+	}
+
 }
