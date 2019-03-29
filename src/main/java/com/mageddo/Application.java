022d8e1c103e860b12482f5817ae87e5e5aac44d
Elvis de Freitas <edigitalb@gmail.com>
2019-03-28 23:45:47 -0300

    setup postgres env by default

--- src/main/java/com/mageddo/Application.java
+++ src/main/java/com/mageddo/Application.java
@@ -9,8 +9,19 @@ import org.graalvm.nativeimage.Feature;
 public class Application implements Feature {
 
 	public static void main(String[] args) {
+
+		setupEnv();
+
 		ApplicationContext ctx = Micronaut.run(Application.class);
 		ApplicationContextUtils.context(ctx);
 		MigrationUtils.migrate(ctx.getEnvironment());
+	}
+
+	static void setupEnv() {
+		final String osEnvs = System.getenv("MICRONAUT_ENVIRONMENTS");
+		final String javaEnvs = System.getProperty("micronaut.environments");
+		if(osEnvs == null && javaEnvs == null){
+			System.setProperty("micronaut.environments", "pg");
+		}
 	}
 }
