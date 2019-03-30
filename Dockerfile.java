b724f3843e0f6f5c8d8c4f44b58b3f48f181ce82
Elvis de Freitas <edigitalb@gmail.com>
2019-03-30 00:15:35 -0300

    fixing docker build

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,12 +1,11 @@
-FROM alpine
+FROM alpine AS BUILDER
 WORKDIR /app
 ENV TMP_NAME=/app/bookmark-notes.zip
 ENV BOOKMARK_NOTES_VERSION=3.0.3
 RUN apk add --update curl &&\
-curl -L "https://github.com/mageddo/bookmark-notes/releases/download/${BOOKMARK_NOTES_VERSION}/bookmark-notes-${BOOKMARK_NOTES_VERSION}.zip" > $TMP_NAME && \
-unzip $TMP_NAME -d /app/
+curl -L "https://github.com/mageddo/bookmark-notes/releases/download/${BOOKMARK_NOTES_VERSION}/bookmark-notes-${BOOKMARK_NOTES_VERSION}.zip" > $TMP_NAME &&\
+unzip $TMP_NAME -d /app && rm $TMP_NAME
 
 FROM debian:9
-COPY --from=BUILDER /app/bookmark-notes.zip /app
-RUN unzip /app/bookmark-notes.zip -d /app && rm /app/bookmark*.zip
-CMD /app/bookmark/bookmark-notes
+COPY --from=BUILDER /app /app
+CMD /app/bookmark-notes/bin/bookmark-notes
