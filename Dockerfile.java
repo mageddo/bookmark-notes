b797850de50d3bbdf6bfce331678e4ae5d4010c4
Elvis de Freitas <edigitalb@gmail.com>
2019-03-29 23:04:22 -0300

    fixing version

--- Dockerfile.java
+++ Dockerfile.java
@@ -3,7 +3,7 @@ WORKDIR /app
 ENV TMP_NAME=/app/bookmark-notes.zip
 ENV BOOKMARK_NOTES_VERSION=3.0.1
 RUN apk add --update curl &&\
-curl -L "https://github.com/mageddo/bookmark-notes/releases/download/${BOOKMARK_NOTES_VERSION}/bookmark-notes-linux-amd64-${BOOKMARK_NOTES_VERSION}.zip" > $TMP_NAME && \
+curl -L "https://github.com/mageddo/bookmark-notes/releases/download/${BOOKMARK_NOTES_VERSION}/bookmark-notes-${BOOKMARK_NOTES_VERSION}.zip" > $TMP_NAME && \
 tar -xvf $TMP_NAME -C /app/ && rm -f $TMP_NAME && apt-get purge --force-yes -y curl
 
 FROM debian:9
