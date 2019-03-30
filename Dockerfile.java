9560ca42da6d9992af44f3d785892efba7e45fff
Travis CI User <travis@example.org>
2019-03-30 03:02:42 +0000

    Releasing 3.0.4

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,7 +1,7 @@
 FROM alpine
 WORKDIR /app
 ENV TMP_NAME=/app/bookmark-notes.zip
-ENV BOOKMARK_NOTES_VERSION=3.0.3
+ENV BOOKMARK_NOTES_VERSION=3.0.4
 RUN apk add --update curl &&\
 curl -L "https://github.com/mageddo/bookmark-notes/releases/download/${BOOKMARK_NOTES_VERSION}/bookmark-notes-${BOOKMARK_NOTES_VERSION}.zip" > $TMP_NAME && \
 unzip $TMP_NAME -d /app/
