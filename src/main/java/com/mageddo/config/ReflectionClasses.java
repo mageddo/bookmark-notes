6f200ee14d8032076bd48925517301f1db356048
Elvis de Freitas <edigitalb@gmail.com>
2019-11-30 23:11:30 -0300

    configuring array using json reflection config

--- src/main/java/com/mageddo/config/ReflectionClasses.java
+++ src/main/java/com/mageddo/config/ReflectionClasses.java
@@ -1,14 +1,11 @@
 package com.mageddo.config;
 
 import com.mageddo.bookmarks.utils.ThymeleafUtils;
-import com.mageddo.common.graalvm.SubstrateVM;
 import com.oracle.svm.core.annotate.AutomaticFeature;
 import nativeimage.Reflection;
 import nativeimage.Reflections;
 import org.flywaydb.core.internal.logging.javautil.JavaUtilLogCreator;
 import org.graalvm.nativeimage.hosted.Feature;
-
-import java.sql.Statement;
 
 @Reflections({
 
@@ -27,7 +24,12 @@ import java.sql.Statement;
 	@Reflection(
 		declaredConstructors = true, declaredMethods = true, declaredFields = true,
 		scanPackage = "com.mageddo.bookmarks.apiserver.res"
-	)
+	),
+
+	/*
+	 	Hikari datasource
+	 */
+	@Reflection(publicConstructors = true, declaredConstructors = true, scanClassName = "java.sql.Statement[]")
 })
 @AutomaticFeature
 class ReflectionClasses implements Feature {
@@ -35,18 +37,6 @@ class ReflectionClasses implements Feature {
 	@Override
 	public void duringSetup(DuringSetupAccess access) {
 		System.loadLibrary("sunec");
-	}
-
-	@Override
-	public void beforeAnalysis(BeforeAnalysisAccess access) {
-
-		// hikari datasource
-		SubstrateVM
-			.builder()
-			.constructors()
-			.clazz(Statement[].class)
-		.build();
-
 	}
 
 }
