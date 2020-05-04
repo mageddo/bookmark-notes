df23ed435619cc03836d06070f150ca9e76a4644
Elvis de Freitas <edigitalb@gmail.com>
2020-05-04 02:17:37 -0300

    fixing native image compile

--- src/main/docker/ci/entrypoint.sh
+++ src/main/docker/ci/entrypoint.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-export JAVA_HOME="${GRAALVM_TARGET_DIR}/graalvm-ce-19.3.0/"
 /app/builder validate-release || exit 0
 /app/builder apply-version && /app/builder build && /app/builder upload-release
 
